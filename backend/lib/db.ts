203ffc8d41310f3d9e21801bfc2734799149c447
vergiLgood1 <xdamazon17@gmail.com>
2024-10-22 22:49:13 +0700

    Update db.ts
    Menambahkan konfigurasi prisma optimize pada prisma client untuk memonitoring performa API

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -1,11 +1,17 @@
 // src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client"
+import { withOptimize } from "@prisma/extension-optimize"
 
 declare global {
-    var db: PrismaClient | undefined;
+  var db: PrismaClient | undefined
 }
 
-const db = global.globalThis.db || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.db = db;
+const db: PrismaClient =
+  global.globalThis.db ||
+  new PrismaClient().$extends(
+    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY || '' })
+  ) as PrismaClient
+  
+if (process.env.NODE_ENV !== "production") globalThis.db = db
 
-export default db;
+export default db
