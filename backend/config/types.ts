2424737840f312b0bc37a98a7d7211f6ba52a032
vergiLgood1 <xdamazon17@gmail.com>
2024-10-22 22:46:48 +0700

    Update types.ts
    Mendefinisikan types  User input dan UserQueryParans

--- backend/config/types.ts
+++ backend/config/types.ts
@@ -1,15 +1,27 @@
 // Interface for user input validation
 export interface UserInput {
-    firstname: string;
-    lastname: string;
-    email: string;
-    password: string;
-    phone_number: string;
-    role: string;
-  }
+  firstname: string
+  lastname: string
+  email: string
+  password: string
+  phone_number: string
+  role: string
+}
 
 // Response types for validation
 export interface ValidationResult {
-    isValid: boolean;
-    error?: string;
-  }
+  isValid: boolean
+  error?: string
+}
+
+export type UserQueryParams = {
+  firstname?: string
+  lastname?: string
+  username?: string
+  email?: string
+  phone?: string
+  role?: string
+  createdAt?: string
+  updateAt?: string
+  [key: string]: string | undefined // Tambahkan tipe yang lebih spesifik
+}
