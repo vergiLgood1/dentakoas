1e6b1e337327162ac8c558efccafcf6a36b5b9c6
vergiLgood1 <xdamazon17@gmail.com>
2024-10-27 22:58:18 +0700

    Update API posts

--- backend/app/api/posts/route.ts
+++ backend/app/api/posts/route.ts
@@ -1,20 +1,18 @@
 import { NextResponse } from "next/server"
 import db from "@/lib/db"
+import { StatusKoas } from "@/config/types"
+import { Prisma } from "@prisma/client"
 
 export async function GET(req: Request) {
-    try {
-        const posts = await db.posts.findMany()
-        return NextResponse.json(posts, { status: 200 })
-    } catch (error) {
-        console.error("Error fetching posts", error)
-        return NextResponse.json(
-        { error: "Internal Server Error" },
-        { status: 500 }
-        )
-    }
+  try {
+    const posts = await db.posts.findMany()
+    return NextResponse.json(posts, { status: 200 })
+  } catch (error) {
+    console.error("Error fetching posts", error)
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    )
+  }
 }
 
-export async function POST(req: Request) {
-    const body = await req.json()
-
-}
