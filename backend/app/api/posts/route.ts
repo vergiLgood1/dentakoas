b01ef3444049d73aa9147eb1592d3dd98a8e6e45
vergiLgood1 <xdamazon17@gmail.com>
2024-10-28 22:27:42 +0700

    Update API posts
    Menambahkan fungsi count pada API posts untuk menghitung jumlah like pada post tertentu

--- backend/app/api/posts/route.ts
+++ backend/app/api/posts/route.ts
@@ -6,7 +6,21 @@ import { Prisma } from "@prisma/client"
 export async function GET(req: Request) {
   try {
     const posts = await db.posts.findMany()
-    return NextResponse.json(posts, { status: 200 })
+
+    if (!posts) {
+      return NextResponse.json({ error: "Posts not found" }, { status: 404 })
+    }
+
+    const likeCount = await db.likes.count()
+
+    const postsWithLikeCount = posts.map((post) => {
+      return {
+        ...post,
+        likeCount,
+      }
+    })
+
+    return NextResponse.json(postsWithLikeCount, { status: 200 })
   } catch (error) {
     console.error("Error fetching posts", error)
     return NextResponse.json(
