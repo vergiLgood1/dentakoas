c39faa61ca87302bb819c5335d5d1d231294a391
vergiLgood1 <xdamazon17@gmail.com>
2024-11-10 16:36:43 +0700

    Create signIn.ts

---
+++ backend/helper/accounts/signIn.ts
@@ -0,0 +1,19 @@
+// import { LoginResponse } from "@/config/types"
+// import db from "@/lib/db"
+// import { NextResponse } from "next/server"
+
+// const signIn = async (params: {
+//   email: string
+//   password: string
+// }): Promise<LoginResponse | null> => {
+//   const user = await db.users.findUnique({
+//     where: { email: params.email },
+//   })
+
+//   if (!user) {
+//     return null
+//   }
+//   return null // Return null if user is not found
+// }
+
+// export default signIn
