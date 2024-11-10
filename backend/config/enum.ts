c09fcdda00b0aa9445698dd2c0786b4c78a649cb
vergiLgood1 <xdamazon17@gmail.com>
2024-11-10 16:18:09 +0700

    Create enum.ts

---
+++ backend/config/enum.ts
@@ -0,0 +1,24 @@
+// Enum for User Role
+export enum Role {
+    Pasien = "Pasien",
+    Koas = "Koas",
+    Admin = "Admin",
+  }
+  
+  // Enum for Gender (if applicable)
+  export enum Gender {
+    Male = "Male",
+    Female = "Female",
+  }
+  
+  export enum StatusPost {
+    Pending = "Pending",
+    Open = "Open",
+    Closed = "Closed",
+  }
+  
+  export enum StatusKoas {
+    Rejected = "Rejected",
+    Pending = "Pending",
+    Approved = "Approved",
+  }
