ff4be074ab5c2cbd5ab014264b21d6613850bb1b
vergiLgood1 <xdamazon17@gmail.com>
2024-11-10 16:18:05 +0700

    Update types.ts

--- backend/config/types.ts
+++ backend/config/types.ts
@@ -1,113 +1,120 @@
-// Enum for User Role
-export enum Role {
-  Pasien = "Pasien",
-  Koas = "Koas",
+import { Gender, Role, StatusKoas, StatusPost } from "@/config/enum"
+
+//  Type for authentication
+export interface LoginResponse {
+  id: string
+  accessToken?: string
+  expiredAt?: string
+  renewalToken?: string
+  user: {
+    email: string
+  }
 }
 
-// Enum for Gender (if applicable)
-export enum Gender {
-  Male = "Male",
-  Female = "Female",
+export interface TokenInfo {
+  expiredAt: string
+  accessToken: string
+  refreshToken: string
+  user: {
+    email: string
+  }
+  error?: string
 }
 
-export enum StatusPost {
-  Pending = "Pending",
-  Open = "Open",
-  Closed = "Closed",
-}
-
-export enum StatusKoas {
-  Rejected = "Rejected",
-  Pending = "Pending",
-  Approved = "Approved",
+export interface SessionInfo {
+  accessToken: string
+  user: {
+    email: string
+  }
+  error?: string
 }
 
 // Type for Users
 export interface User {
-  id: string;
-  firstname: string;
-  lastname: string;
-  username: string;
-  email: string;
-  password: string;
-  phone: string;
-  img?: string | null; // Optional field
-  role: Role;
-  koasProfile?: KoasProfile | null; // Optional field
-  pasienProfile?: PasienProfile | null; // Optional field
-  createdAt: Date;
-  updateAt: Date;
+  id: string
+  firstname: string
+  lastname: string
+  username: string
+  email: string
+  password: string
+  phone: string
+  img?: string | null // Optional field
+  role: Role
+  koasProfile?: KoasProfile | null // Optional field
+  pasienProfile?: PasienProfile | null // Optional field
+  createdAt: Date
+  updateAt: Date
 }
 
 // Type for KoasProfile
 export interface KoasProfile {
-  id: string;
-  userId: string;
-  koasNumber?: string | null; // Optional field
-  faculty?: string | null; // Optional field
-  bio?: string | null; // Optional field
-  whatsappLink?: string | null; // Optional field
-  status: StatusKoas;
-  createdAt: Date;
-  updateAt: Date;
+  id: string
+  userId: string
+  koasNumber?: string | null // Optional field
+  faculty?: string | null // Optional field
+  bio?: string | null // Optional field
+  whatsappLink?: string | null // Optional field
+  status: StatusKoas
+  createdAt: Date
+  updateAt: Date
 }
 
 // Type for PasienProfile
 export interface PasienProfile {
-  id: string;
-  userId: string;
-  age?: number | null; // Optional field
-  gender?: Gender | null; // Optional field
-  bio?: string | null; // Optional field
-  createdAt: Date;
-  updateAt: Date;
+  id: string
+  userId: string
+  age?: number | null // Optional field
+  gender?: Gender | null // Optional field
+  bio?: string | null // Optional field
+  createdAt: Date
+  updateAt: Date
 }
 
 // Type for Posts
 export type Post = {
-  id: string;
-  userId: string;
-  koasId: string;
-  treatmentId: string;
-  title: string;
-  desc: string;
-  patientRequirement?: string;
-  status: StatusPost;
-  published: boolean;
-  createdAt: Date;
-  updateAt: Date;
-  koas?: KoasProfile;
-  users?: User;
-  likes?: Like[];
-};
+  id: string
+  userId: string
+  koasId: string
+  treatmentId: string
+  title: string
+  desc: string
+  patientRequirement?: string
+  status: StatusPost
+  published: boolean
+  createdAt: Date
+  updateAt: Date
+  koas?: KoasProfile
+  users?: User
+  likes?: Like[]
+}
 
 // Type for Likes
 export type Like = {
-  id: string;
-  postId: string;
-  userId: string;
-  createdAt: Date;
-  user?: User;
-  posts?: Post;
-};
+  id: string
+  postId: string
+  userId: string
+  createdAt: Date
+  user?: User
+  posts?: Post
+}
 
 // Type for Notifications
 export type Notification = {
-  id: string;
-  userId: string;
-  message: string;
-  isRead: boolean;
-  createdAt: Date;
-  user?: User;
-};
+  id: string
+  userId: string
+  message: string
+  isRead: boolean
+  createdAt: Date
+  user?: User
+}
 
 // Type for TreatmentType
 export type TreatmentType = {
-  id: string;
-  name: string;
-  createdAt: Date;
-  updateAt: Date;
-};
+  id: string
+  name: string
+  createdAt: Date
+  updateAt: Date
+}
 
 // Interface for user input validation
 export interface UserInput {
