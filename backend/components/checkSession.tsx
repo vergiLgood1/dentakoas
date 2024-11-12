3fe8c149b654cd34c691a64ddff6fbe4383e2ebd
vergiLgood1 <xdamazon17@gmail.com>
2024-11-12 23:21:20 +0700

    Update checkSession.tsx
    Membuat page session

--- backend/components/checkSession.tsx
+++ backend/components/checkSession.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const CheckSession = async () => {
   const session = await auth();
@@ -16,6 +16,20 @@ const CheckSession = async () => {
         {session ? (
           <div>
             <p>{JSON.stringify(session, null, 2)}</p>
+            <form
+              action={async () => {
+                "use server";
+
+                await signOut();
+              }}
+            >
+              <button
+                type="submit"
+                className="flex w-full items-center text-center justify-center px-4 py-2 mt-4 text-white bg-red-500 rounded-md"
+              >
+                Sign Out
+              </button>
+            </form>
           </div>
         ) : (
           <p>No active session found.</p>
