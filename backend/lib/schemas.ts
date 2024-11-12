fd553b9f82b27d98fe4c52ea89205c7a8a67915b
vergiLgood1 <xdamazon17@gmail.com>
2024-11-12 23:21:05 +0700

    Create schemas.ts
    Membuat file schemas untuk sebagai file yang aakn menyimpan semua schemas pada api

---
+++ backend/lib/schemas.ts
@@ -0,0 +1,39 @@
+import * as z from "zod";
+import { NextResponse } from "next/server";
+
+export const SignUpSchema = z
+  .object({
+    firstname: z.string().min(1, "Firstname must be more than 1 character"),
+    lastname: z.string().min(1, "Lastname must be more than 1 character"),
+    email: z.string().email({
+      message: "Invalid email format",
+    }),
+    password: z
+      .string()
+      .min(8, "Password must be more than 8 characters")
+      .max(32, "Password must be less than 3 2characters"),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be more than 8 characters")
+      .max(32, "Password must be less than 32 characters"),
+    role: z.enum(["Koas", "Pasien", "Admin"]),
+    phone_number: z.string().min(10).optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password does not match",
+    path: ["confirmPassword"],
+  });
+
+export const SignInSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be more than 8 characters",
+    })
+    .max(32, {
+      message: "Password must be less than 32 characters",
+    }),
+});
