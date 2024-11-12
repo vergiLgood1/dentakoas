639619267f3faf9566e2caaf0abeb23ebad17265
vergiLgood1 <xdamazon17@gmail.com>
2024-11-12 20:58:34 +0700

    Update middleware.ts
    set the middleware

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
-import { NextRequest } from "next/server";
 
 import {
   DEFAULT_LOGIN_REDIRECT,
+  DEFAULT_PROTECTED_REDIRECT,
   apiAuthPrefix,
   authRoutes,
   publicRoutes,
@@ -11,13 +11,30 @@ import {
 
 const { auth } = NextAuth(authConfig);
 
-export default auth(async function middleware(req) {
+export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL(DEFAULT_PROTECTED_REDIRECT, nextUrl));
+  }
+
+  return;
 });
 
 export const config = {
