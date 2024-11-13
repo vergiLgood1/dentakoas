58c369eb5f930af5cf80b3a92a5dfdfae1291413
vergiLgood1 <xdamazon17@gmail.com>
2024-11-13 23:13:23 +0700

    Update user.ts
    Menambahkan docs js pada reusable functions

--- backend/helpers/user.ts
+++ backend/helpers/user.ts
@@ -2,6 +2,13 @@ import { UserQueryParams } from "@/config/types";
 import db from "@/lib/db";
 import bcrpyt from "bcryptjs";
 
+/**
+ * Get user by email from the database
+ * @param email
+ * @param include
+ * @param select
+ * @returns
+ */
 export const getUserByEmail = async (
   email: string,
   include?: object,
@@ -21,6 +28,13 @@ export const getUserByEmail = async (
   }
 };
 
+/**
+ * Get user by ID from the database
+ * @param id
+ * @param include
+ * @param select
+ * @returns
+ */
 export const getUserById = async (
   id: string,
   include?: object,
@@ -40,7 +54,11 @@ export const getUserById = async (
   }
 };
 
-// Fungsi untuk memproses searchParams dan mengonversinya menjadi query object
+/**
+ * Parse the search params from the URL and return an object
+ * @param searchParams
+ * @returns
+ */
 export function parseSearchParams(
   searchParams: URLSearchParams
 ): UserQueryParams {
@@ -53,6 +71,12 @@ export function parseSearchParams(
   return query;
 }
 
+/**
+ * Generate a hashed password or return the existing hash if the password is not provided
+ * @param password
+ * @param existingHash
+ * @returns
+ */
 export async function setHashPassword(
   password: string | undefined,
   existingHash: string
@@ -60,7 +84,16 @@ export async function setHashPassword(
   return password ? await bcrpyt.hash(password, 12) : existingHash;
 }
 
-export async function setUsername(firstname: string | null, lastname: string | null) {
+/**
+ * Generate a username based on the user's first and last name
+ * @param firstname
+ * @param lastname
+ * @returns
+ */
+export async function genUsername(
+  firstname: string | null,
+  lastname: string | null
+) {
   const rand = Math.floor(Math.random() * 1000);
   return `${firstname?.toLowerCase()}.${lastname?.toLowerCase()}${rand}`;
 }
