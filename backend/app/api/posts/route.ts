f365cc50dba24fd7bd5db523b6df91863602cba9
vergiLgood1 <xdamazon17@gmail.com>
2024-11-27 21:47:43 +0700

    Update route.ts

--- backend/app/api/posts/route.ts
+++ backend/app/api/posts/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import db from "@/lib/db";
-import { PostQueryString } from "@/config/types";
 import { parseSearchParamsPost } from "@/helpers/post";
 import { Prisma } from "@prisma/client";
+import { StatusKoas } from "@/config/enum";
+import { genSchedules, genTimeSlots } from "@/utils/dateTime";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -14,6 +15,23 @@ export async function GET(req: Request) {
         ...queStr,
       } as Prisma.PostWhereInput,
       include: {
+        Schedule: {
+          select: {
+            id: true,
+            dateStart: true,
+            dateEnd: true,
+            timeslot: {
+              select: {
+                id: true,
+                startTime: true,
+                endTime: true,
+                maxParticipants: true,
+                currentParticipants: true,
+                isAvailable: true,
+              },
+            },
+          },
+        },
         _count: {
           select: { likes: true }, // Menghitung jumlah likes untuk setiap post
         },
@@ -42,6 +60,104 @@ export async function GET(req: Request) {
     console.error("Error fetching Posts", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const {
+    userId,
+    koasId,
+    treatmentId,
+    title,
+    desc,
+    patientRequirement,
+    requiredParticipant,
+    dateRangeStart,
+    dateRangeEnd,
+    published,
+    status,
+    koasStartTime, // Waktu mulai koas
+    koasEndTime, // Waktu selesai koas
+  } = body;
+
+  try {
+    if (!userId || !koasId || !treatmentId || !title || !desc) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const koasProfile = await db.koasProfile.findUnique({
+      where: { id: koasId },
+      select: {
+        status: true,
+      },
+    });
+
+    if (!koasProfile) {
+      return NextResponse.json(
+        { error: "Koas Profile not found" },
+        { status: 404 }
+      );
+    }
+
+    if (koasProfile.status !== StatusKoas.Approved) {
+      return NextResponse.json(
+        { error: "Don't have access to post before status approved" },
+        { status: 400 }
+      );
+    }
+
+    // Membuat Post terlebih dahulu
+    const post = await db.post.create({
+      data: {
+        title,
+        desc,
+        patientRequirement,
+        requiredParticipant,
+        status,
+        published,
+        user: { connect: { id: String(userId) } },
+        koas: { connect: { id: String(koasId) } },
+        treatmentId: String(treatmentId),
+      } as Prisma.PostCreateInput,
+    });
+
+    // // Generate jadwal berdasarkan dateRangeStart hingga dateRangeEnd
+    // const scheduleDates = genSchedules(
+    //   new Date(dateRangeStart),
+    //   new Date(dateRangeEnd)
+    // );
+
+    // // Create schedules first using createMany
+    // const schedulesData = scheduleDates.map((scheduleDate) => ({
+    //   date: scheduleDate,
+    //   postId: post.id,
+    // }));
+
+    // await db.schedule.createMany({
+    //   data: schedulesData,
+    // });
+
+    return NextResponse.json(
+      {
+        status: "Success",
+        message: "Posts retrieved successfully",
+        data: { posts: post },
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "An error occurred while creating the post" },
       { status: 500 }
     );
   }
