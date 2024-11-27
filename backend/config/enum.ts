440744418c84cdc6937be466b972ea5ee81dd44e
vergiLgood1 <xdamazon17@gmail.com>
2024-11-27 21:47:22 +0700

    Update enum.ts
    menambahkan enum baru

--- backend/config/enum.ts
+++ backend/config/enum.ts
@@ -1,24 +1,30 @@
 // Enum for User Role
 export enum Role {
-    Pasien = "Pasien",
-    Koas = "Koas",
-    Admin = "Admin",
-  }
-  
-  // Enum for Gender (if applicable)
-  export enum Gender {
-    Male = "Male",
-    Female = "Female",
-  }
-  
-  export enum StatusPost {
-    Pending = "Pending",
-    Open = "Open",
-    Closed = "Closed",
-  }
-  
-  export enum StatusKoas {
-    Rejected = "Rejected",
-    Pending = "Pending",
-    Approved = "Approved",
-  }
+  Pasien = "Pasien",
+  Koas = "Koas",
+  Admin = "Admin",
+}
+
+// Enum for Gender (if applicable)
+export enum Gender {
+  Male = "Male",
+  Female = "Female",
+}
+
+export enum StatusPost {
+  Pending = "Pending",
+  Open = "Open",
+  Closed = "Closed",
+}
+
+export enum StatusKoas {
+  Rejected = "Rejected",
+  Pending = "Pending",
+  Approved = "Approved",
+}
+
+export enum StatusAppointment {
+  Waiting = "Waiting",
+  Ongoing = "Ongoing",
+  Completed = "Completed",
+}
