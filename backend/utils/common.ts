ea037b3f2b4a80237b77081d4a2fc466c5f7dd0d
vergiLgood1 <xdamazon17@gmail.com>
2024-11-28 23:49:20 +0700

    Update common.ts
    Menambahkan function getFieldById

--- backend/utils/common.ts
+++ backend/utils/common.ts
@@ -1,5 +1,6 @@
 import { prismaModels } from "@/config/const";
 import { TableName } from "@/config/types";
+import db from "@/lib/db";
 
 export async function findLastRecord<T extends TableName>(
   tableName: T,
@@ -37,4 +38,34 @@ export async function genId<T extends TableName>(
   const formattedNumber = nextNumber.toString().padStart(4, "0");
 
   return `${prefix}${formattedNumber}`;
+}
+
+export async function getFieldById<T extends TableName>(
+  tableName: T,
+  id: string,
+  include?: object,
+  select?: object
+) {
+  try {
+    // Validasi: Tidak boleh menggunakan `select` dan `include` bersamaan
+    if (select && include) {
+      throw new Error(
+        "Cannot use both `select` and `include` at the same time"
+      );
+    }
+
+    const model = prismaModels[tableName];
+
+    const user = await (model as any).findUnique({
+      where: {
+        id,
+      },
+      ...(include ? { include } : {}), // Gunakan include jika ada
+      ...(select ? { select } : {}), // Gunakan select jika ada
+    });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
 }
