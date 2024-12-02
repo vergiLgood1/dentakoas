06656347243576a54b7c63783ff1bef719b5fd4e
vergiLgood1 <xdamazon17@gmail.com>
2024-12-02 21:49:15 +0700

    Update middleware.ts
    Memperbarui middleware untuk menangani role based access

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -4,6 +4,7 @@ import authConfig from "./auth.config";
 import {
   DEFAULT_LOGIN_REDIRECT,
   DEFAULT_PROTECTED_REDIRECT,
+  ROUTE_ROLES,
   apiAuthPrefix,
   apiPublicRoutes,
   authRoutes,
@@ -16,26 +17,34 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+  const userRole = req.auth?.user.role;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isApiPublicRoute = apiPublicRoutes.includes(nextUrl.pathname);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  // if (isApiAuthRoute || isApiPublicRoute) {
-  //   return;
-  // }
+  // Role-Based-Access Control
+  const routeRoles = userRole ? ROUTE_ROLES[userRole] : undefined;
 
-  // if (isAuthRoute) {
-  //   if (isLoggedIn) {
-  //     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-  //   }
-  //   return;
-  // }
+  if (routeRoles?.length && !routeRoles.includes(nextUrl.pathname)) {
+    return NextResponse.redirect(new URL(DEFAULT_PROTECTED_REDIRECT, nextUrl));
+  }
 
-  // if (!isLoggedIn && !isPublicRoute && !isApiPublicRoute) {
-  //   return NextResponse.redirect(new URL(DEFAULT_PROTECTED_REDIRECT, nextUrl));
-  // }
+  if (isApiAuthRoute || isApiPublicRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL(DEFAULT_PROTECTED_REDIRECT, nextUrl));
+  }
 
   return;
 });
