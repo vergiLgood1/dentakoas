644f6a0ea159b5a475e7708c39081c492d60f1f5
vergiLgood1 <xdamazon17@gmail.com>
2024-12-02 21:37:39 +0700

    Create treatment-type.ts
    add data treatmentTypes

---
+++ backend/data/treatment-type.ts
@@ -0,0 +1,62 @@
+export const TreatmentTypes = [
+  {
+    name: "Pembersihan Karang Gigi (Scaling)",
+  },
+  {
+    name: "Penambalan Gigi",
+  },
+  {
+    name: "Pencabutan Gigi",
+  },
+  {
+    name: "Perawatan Saluran Akar (Endodontik)",
+  },
+  {
+    name: "Pemasangan Kawat Gigi Sementara",
+  },
+  {
+    name: "Pembuatan Gigi Palsu (Prosthodontik)",
+  },
+  {
+    name: "Perawatan Periodontal",
+  },
+  {
+    name: "Perawatan Estetik (Bleaching atau Veneer)",
+  },
+  {
+    name: "Pemasangan Mahkota Gigi (Crown)",
+  },
+  {
+    name: "Operasi Gigi Bungsu",
+  },
+  {
+    name: "Penghalusan Akar Gigi (Root Planing)",
+  },
+  {
+    name: "Perawatan Ortodontik (Aligner)",
+  },
+  {
+    name: "Perawatan Gigi Anak (Pediatrik)",
+  },
+  {
+    name: "Pembuatan Inlay atau Onlay",
+  },
+  {
+    name: "Perawatan Gigi Sensitif",
+  },
+  {
+    name: "Pemeriksaan Rutin dan Konsultasi",
+  },
+  {
+    name: "Pemasangan Sealant Gigi",
+  },
+  {
+    name: "Perawatan Luka atau Trauma pada Gigi",
+  },
+  {
+    name: "Penghapusan Gigi Tiruan yang Rusak",
+  },
+  {
+    name: "Rekonstruksi Gigi Patah",
+  },
+];
