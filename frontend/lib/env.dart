03f4aab77353412a32ed6da7e5c0a8af759c5094
vergiLgood1 <xdamazon17@gmail.com>
2024-12-08 23:39:00 +0700

    Update env.dart

--- frontend/lib/env.dart
+++ frontend/lib/env.dart
@@ -2,4 +2,10 @@ class Env {
   static const String baseUrl = bool.fromEnvironment('dart.vm.product')
       ? 'https://localhost:3000/api'
       : 'https://localhost:3000/api';
+
+  static const String contentType = 'Content-Type';
+  static const String applicationType = 'application/json';
+  static const String accept = 'Accept';
+  static const String applicationJson = 'application/json';
+  static const Duration apiTimeOut = Duration(milliseconds: 10000);
 }
