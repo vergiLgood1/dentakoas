07875d197262cb3e51b2ab7d238567c4a3d64d59
vergiLgood1 <xdamazon17@gmail.com>
2024-12-08 23:40:44 +0700

    Create api_response.dart

---
+++ frontend/lib/src/network/api_response.dart
@@ -0,0 +1,22 @@
+class ApiResponse<T> {
+  final bool success;
+  final T? data;
+  final String? message;
+
+  ApiResponse({
+    required this.success,
+    this.data,
+    this.message,
+  });
+
+  factory ApiResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(dynamic data) fromJsonT,
+  ) {
+    return ApiResponse(
+      success: json['success'] ?? false,
+      data: json['data'] != null ? fromJsonT(json['data']) : null,
+      message: json['message'],
+    );
+  }
+}
