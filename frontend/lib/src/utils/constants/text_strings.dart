3d33bfb1518e463f94b8cdfaa2403fe2d77b5f50
vergiLgood1 <xdamazon17@gmail.com>
2024-12-16 21:31:38 +0700

    Update text_strings.dart

--- frontend/lib/src/utils/constants/text_strings.dart
+++ frontend/lib/src/utils/constants/text_strings.dart
@@ -53,15 +53,22 @@ class TTexts {
   static const String signupTitle = "Let’s create your account";
   static const String forgetPasswordTitle = "Forget password";
   static const String forgetPasswordSubTitle = "Don’t worry sometimes people can forget too, enter your email and we will send you a password reset link.";
-  static const String changeYourPasswordTitle = "Password Reset Email Sent";
-  static const String changeYourPasswordSubTitle = "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
+  static const String verificationCodeTitle = "Enter Verification Code";
+  static const String verficationCodeSubtitle =
+      "Verification code has been e-mailed to dentakoas@example.com.";
   static const String confirmEmail = "Verify your email address!";
   static const String confirmEmailSubTitle = "Congratulations! Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
   static const String emailNotReceivedMessage = "Didn’t get the email? Check your junk/spam or resend it.";
   static const String yourAccountCreatedTitle = "Your account successfully created!";
   static const String yourAccountCreatedSubTitle = "Welcome to Your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping!";
 
+  static const String notReceivingCode = "not receiving code?";
+  static const String resendCode = "Resend Code";
 
+  static const String resetYourPasswordTitle = "Reset Your Password";
+  static const String resetYourPasswordSubTitle =
+      "Enter your new password and confirm it to reset your password.";
+  
   // -- Product
   static const String popularProducts = "Popular Products";
 
