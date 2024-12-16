a393cf135dc755b05d300b966cd75a0ce8e98835
vergiLgood1 <xdamazon17@gmail.com>
2024-12-16 21:30:25 +0700

    Create forgot_password.dart
    add forget password screen

---
+++ frontend/lib/src/features/authentication/presentasion/password_configurations/forgot_password.dart
@@ -0,0 +1,54 @@
+import 'package:denta_koas/src/features/authentication/presentasion/password_configurations/verification_code.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
+import 'package:denta_koas/src/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Headings
+            Text(
+              TTexts.forgetPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              TTexts.forgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections * 2),
+
+            // Text Field
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: TTexts.email,
+                prefixIcon: Icon(Iconsax.direct_right),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            // Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => Get.off(() => const VerificationCodeScreen()),
+                child: const Text(TTexts.submit),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
