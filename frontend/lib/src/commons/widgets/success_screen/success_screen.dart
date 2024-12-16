a7089a1d522c063bb75bae44251de85101fe2459
vergiLgood1 <xdamazon17@gmail.com>
2024-12-16 17:01:24 +0700

    Create success_screen.dart
    addreusable success screen

---
+++ frontend/lib/src/commons/widgets/success_screen/success_screen.dart
@@ -0,0 +1,63 @@
+import 'package:denta_koas/src/commons/styles/spacing_styles.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
+import 'package:denta_koas/src/utils/constants/text_strings.dart';
+import 'package:denta_koas/src/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    this.onPressed,
+  });
+
+  final String image, title, subtitle;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: DSpacingStyle.paddingWithAppBarHeight * 2,
+          child: Column(
+            children: [
+              // Image
+              Image(
+                image: AssetImage(image),
+                width: THelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Title & subtitle
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Text(
+                subtitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text(TTexts.tContinue),
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
