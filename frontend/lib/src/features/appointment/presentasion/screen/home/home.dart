5839c2fb0008fca6b01c0a7746b2d98b95badd4c
vergiLgood1 <xdamazon17@gmail.com>
2024-12-18 22:28:40 +0700

    Create home.dart

---
+++ frontend/lib/src/features/appointment/presentasion/screen/home/home.dart
@@ -0,0 +1,51 @@
+import 'package:denta_koas/src/commons/styles/spacing_styles.dart';
+import 'package:denta_koas/src/features/appointment/presentasion/widgets/cards/appointment_card.dart';
+import 'package:denta_koas/src/features/appointment/presentasion/widgets/categories/category_section.dart';
+import 'package:denta_koas/src/features/appointment/presentasion/widgets/header/search_bar.dart';
+import 'package:denta_koas/src/features/appointment/presentasion/widgets/header/topbar.dart';
+import 'package:denta_koas/src/features/appointment/presentasion/widgets/upcoming/upcoming_event.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: DSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // header
+              WelcomeCard(),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              AppointmentCards(
+                imgUrl: 'https://example.com/image.jpg',
+                name: 'Dr. John Doe',
+                category: 'Dentist',
+                date: 'Sunday, 12 June',
+                timestamp: '10:00 - 11:00 AM',
+              ),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              // Search Bar
+              SearchBarApp(),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              // Categories
+              CategorySection(),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              // Find Upcoming Koas
+              FindUpcomingKoas(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
