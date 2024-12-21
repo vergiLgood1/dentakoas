120bd3ac6f6cb6f39ed76d29121b6b35739f1acf
vergiLgood1 <xdamazon17@gmail.com>
2024-12-21 23:31:14 +0700

    Create doctor_title_text.dart

---
+++ frontend/lib/src/commons/widgets/text/doctor_title_text.dart
@@ -0,0 +1,35 @@
+import 'package:denta_koas/src/utils/constants/enums.dart';
+import 'package:flutter/material.dart';
+
+class DoctorTitleText extends StatelessWidget {
+  const DoctorTitleText({
+    super.key,
+    this.color,
+    required this.title,
+    this.maxLines = 1,
+    this.textAlign = TextAlign.center,
+    this.doctorTextSize = TextSizes.small,
+  });
+
+  final Color? color;
+  final String title;
+  final int maxLines;
+  final TextAlign? textAlign;
+  final TextSizes doctorTextSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      maxLines: maxLines,
+      textAlign: textAlign,
+      overflow: TextOverflow.ellipsis,
+      // Check which doctorTextSize is required and set that style.
+      style: doctorTextSize == TextSizes.small
+          ? Theme.of(context).textTheme.labelMedium!.apply(color: color)
+          : doctorTextSize == TextSizes.medium
+              ? Theme.of(context).textTheme.titleLarge!.apply(color: color)
+              : Theme.of(context).textTheme.bodyMedium!.apply(color: color),
+    );
+  }
+}
