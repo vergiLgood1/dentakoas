65ceb0860dae2c9917412107b57c7b92f6398476
vergiLgood1 <xdamazon17@gmail.com>
2024-12-21 23:31:08 +0700

    Create circular_image.dart

---
+++ frontend/lib/src/commons/widgets/images/circular_image.dart
@@ -0,0 +1,46 @@
+import 'package:denta_koas/src/utils/constants/colors.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
+import 'package:denta_koas/src/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class CircularImage extends StatelessWidget {
+  const CircularImage({
+    super.key,
+    this.fit,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.widht = 56,
+    this.height = 56,
+    this.padding = TSizes.sm,
+    this.radius = 100,
+  });
+
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double widht, height, padding, radius;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Container(
+      width: widht,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: backgroundColor ?? (dark ? TColors.black : TColors.light),
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      child: Image(
+        image: isNetworkImage ? NetworkImage(image) : AssetImage(image),
+        color: overlayColor,
+        fit: fit,
+      ),
+    );
+  }
+}
