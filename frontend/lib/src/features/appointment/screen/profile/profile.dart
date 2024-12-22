165d7e604a68922db17a58fb13ffad7eede57e13
vergiLgood1 <xdamazon17@gmail.com>
2024-12-22 22:23:06 +0700

    Update profile.dart

--- frontend/lib/src/features/appointment/screen/profile/profile.dart
+++ frontend/lib/src/features/appointment/screen/profile/profile.dart
@@ -1,6 +1,5 @@
 import 'package:denta_koas/src/commons/widgets/appbar/appbar.dart';
 import 'package:denta_koas/src/features/appointment/screen/profile/widgets/avatar.dart';
-import 'package:denta_koas/src/features/appointment/screen/profile/widgets/profile_menus.dart';
 import 'package:denta_koas/src/utils/constants/colors.dart';
 import 'package:denta_koas/src/utils/constants/image_strings.dart';
 import 'package:denta_koas/src/utils/constants/sizes.dart';
