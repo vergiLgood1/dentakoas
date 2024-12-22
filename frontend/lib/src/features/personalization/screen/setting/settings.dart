6583ec4bb61030819666365915c38ca86c52f346
vergiLgood1 <xdamazon17@gmail.com>
2024-12-22 23:32:44 +0700

    Update settings.dart

--- frontend/lib/src/features/personalization/screen/setting/settings.dart
+++ frontend/lib/src/features/personalization/screen/setting/settings.dart
@@ -1,10 +1,112 @@
+import 'package:denta_koas/src/commons/widgets/appbar/appbar.dart';
+import 'package:denta_koas/src/commons/widgets/containers/primary_header_container.dart';
+import 'package:denta_koas/src/commons/widgets/list_tiles/setting_menu_tile.dart';
+import 'package:denta_koas/src/commons/widgets/list_tiles/user_profile_tile.dart';
+import 'package:denta_koas/src/commons/widgets/text/section_heading.dart';
+import 'package:denta_koas/src/utils/constants/colors.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  // AppBar
+                  DAppBar(
+                    title: Text(
+                      'Account',
+                      style: Theme.of(context).textTheme.headlineMedium!.apply(
+                            color: TColors.textWhite,
+                          ),
+                    ),
+                  ),
+
+                  // User Profile Card
+                  const UserProfileTile(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                ],
+              ),
+            ),
+
+            // Body
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  // Account settings
+                  const SectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+
+                  const SettingMenuTile(
+                    icon: Iconsax.safe_home,
+                    title: 'My Address',
+                    subtitle: 'Set your default address',
+                  ),
+                  const SettingMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Appointments',
+                    subtitle: 'View your appointments',
+                  ),
+                  const SettingMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notifications',
+                    subtitle: 'Set any kind of notifications',
+                  ),
+                  const SettingMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subtitle: 'Control your privacy settings',
+                  ),
+                  const SettingMenuTile(
+                    icon: Iconsax.user,
+                    title: 'Change Account',
+                    subtitle: 'Change your account ',
+                  ),
+
+                  // App settings
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  const SectionHeading(
+                    title: 'App Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+                  SettingMenuTile(
+                    icon: Iconsax.location,
+                    title: 'Geolocation',
+                    subtitle: 'Set recomendarions based on your location',
+                    trailing: Switch(value: true, onChanged: (value) {}),
+                  ),
+
+                  // Logout button
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
