913fb297b719d2ff6171d7771c1a25560ac65046
vergiLgood1 <xdamazon17@gmail.com>
2024-12-22 23:32:31 +0700

    Create user_profile_tile.dart

---
+++ frontend/lib/src/commons/widgets/list_tiles/user_profile_tile.dart
@@ -0,0 +1,40 @@
+import 'package:denta_koas/src/commons/widgets/images/circular_image.dart';
+import 'package:denta_koas/src/utils/constants/colors.dart';
+import 'package:denta_koas/src/utils/constants/image_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class UserProfileTile extends StatelessWidget {
+  const UserProfileTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const CircularImage(
+        image: TImages.user,
+        widht: 50,
+        height: 50,
+        padding: 0,
+      ),
+      title: Text(
+        'Denta App',
+        style: Theme.of(context)
+            .textTheme
+            .headlineSmall!
+            .apply(color: TColors.textWhite),
+      ),
+      subtitle: Text(
+        'support@dentaapp.com',
+        style: Theme.of(context)
+            .textTheme
+            .bodyMedium!
+            .apply(color: TColors.textWhite),
+      ),
+      trailing: IconButton(
+        onPressed: () {},
+        icon: const Icon(Iconsax.edit),
+        color: TColors.white,
+      ),
+    );
+  }
+}
