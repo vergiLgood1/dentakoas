1bea91c9b8c2e56001e10f35b48d113bd1c7d3d8
vergiLgood1 <xdamazon17@gmail.com>
2024-12-22 23:32:28 +0700

    Create setting_menu_tile.dart

---
+++ frontend/lib/src/commons/widgets/list_tiles/setting_menu_tile.dart
@@ -0,0 +1,33 @@
+import 'package:denta_koas/src/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class SettingMenuTile extends StatelessWidget {
+  const SettingMenuTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.trailing,
+    this.onTap,
+  });
+
+  final IconData icon;
+  final String title, subtitle;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 28,
+        color: TColors.primary,
+      ),
+      title: Text(title, style: Theme.of(context).textTheme.titleMedium),
+      subtitle: Text(subtitle, style: Theme.of(context).textTheme.labelMedium),
+      trailing: trailing,
+      onTap: onTap,
+    );
+  }
+}
