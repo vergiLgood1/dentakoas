ee66b7a5182933ea3509860f8f2ad84e3b6808eb
vergiLgood1 <xdamazon17@gmail.com>
2024-12-22 22:23:04 +0700

    Update explore.dart

--- frontend/lib/src/features/appointment/screen/explore/explore.dart
+++ frontend/lib/src/features/appointment/screen/explore/explore.dart
@@ -1,12 +1,13 @@
 import 'package:denta_koas/src/commons/widgets/appbar/appbar.dart';
-import 'package:denta_koas/src/commons/widgets/containers/rounded_container.dart';
+import 'package:denta_koas/src/commons/widgets/appbar/tabbar.dart';
+import 'package:denta_koas/src/commons/widgets/cards/partner_card.dart';
 import 'package:denta_koas/src/commons/widgets/containers/search_container.dart';
 import 'package:denta_koas/src/commons/widgets/layouts/grid_layout.dart';
 import 'package:denta_koas/src/commons/widgets/notifications/notification_menu.dart';
-import 'package:denta_koas/src/commons/widgets/text/doctor_title_with_verified.dart';
+import 'package:denta_koas/src/commons/widgets/partnert/partner_showcase.dart';
 import 'package:denta_koas/src/commons/widgets/text/section_heading.dart';
+import 'package:denta_koas/src/features/appointment/screen/home/widgets/cards/doctor_card.dart';
 import 'package:denta_koas/src/utils/constants/colors.dart';
-import 'package:denta_koas/src/utils/constants/enums.dart';
 import 'package:denta_koas/src/utils/constants/image_strings.dart';
 import 'package:denta_koas/src/utils/constants/sizes.dart';
 import 'package:denta_koas/src/utils/helpers/helper_functions.dart';
@@ -19,99 +20,136 @@ class ExploreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
-    return Scaffold(
-      appBar: DAppBar(
-        title:
-            Text('Explore', style: Theme.of(context).textTheme.headlineMedium),
-        actions: [
-          NotificationCounterIcon(onPressed: () {}),
-        ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: DAppBar(
+          title: Text('Explore',
+              style: Theme.of(context).textTheme.headlineMedium),
+          actions: [
+            NotificationCounterIcon(onPressed: () {}),
+          ],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (_, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                floating: true,
+                backgroundColor: dark ? TColors.black : TColors.white,
+                expandedHeight: 440,
+                flexibleSpace: Padding(
+                  padding: const EdgeInsets.all(TSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      // Search bar
+                      const SizedBox(height: TSizes.spaceBtwItems),
+                      const SearchContainer(
+                        text: 'Search something...',
+                        showBackground: false,
+                        padding: EdgeInsets.zero,
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      // Featured koas
+                      SectionHeading(
+                          title: 'Featured Partner', onPressed: () {}),
+                      const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                      DGridLayout(
+                        itemCount: 4,
+                        mainAxisExtent: 80,
+                        itemBuilder: (_, index) {
+                          return const PartnertCard(
+                            title: 'Apple',
+                            subtitle: 'Dentist',
+                            image: TImages.appleLogo,
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+
+                // Tabs
+                bottom: const TabBarApp(tabs: [
+                  Tab(text: 'Posts'),
+                  Tab(text: 'Dentist'),
+                  Tab(text: 'Partners'),
+                ]),
+              ),
+            ];
+          },
+          body: const TabBarView(
+            children: [
+              CategoryTab(),
+              CategoryTab(),
+              CategoryTab(),
+            ],
+          ),
+        ),
       ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true,
-              floating: true,
-              backgroundColor: dark ? TColors.black : TColors.white,
-              expandedHeight: 440,
-              flexibleSpace: Padding(
-                padding: const EdgeInsets.all(TSizes.defaultSpace),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    // Search bar
-                    const SizedBox(height: TSizes.spaceBtwItems),
-                    const SearchContainer(
-                      text: 'Search something...',
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+    );
+  }
+}
 
-                    // Featured koas
-                    SectionHeading(title: 'Featured Partner', onPressed: () {}),
-                    const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+class CategoryTab extends StatelessWidget {
+  const CategoryTab({
+    super.key,
+  });
 
-                    DGridLayout(
-                      itemCount: 4,
-                      mainAxisExtent: 80,
-                      itemBuilder: (_, index) {
-                        return GestureDetector(
-                          onTap: () {},
-                          child: RoundedContainer(
-                            padding: const EdgeInsets.all(TSizes.sm),
-                            showBorder: true,
-                            backgroundColor: TColors.transparent,
-                            child: Row(
-                              children: [
-                                const Flexible(
-                                  child: CircleAvatar(
-                                    radius: 24,
-                                    backgroundColor: TColors.transparent,
-                                    backgroundImage:
-                                        AssetImage(TImages.appleLogo),
-                                  ),
-                                ),
-                                const SizedBox(width: TSizes.spaceBtwItems / 2),
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              // Partners showcase
+              const PartnertShowcase(
+                images: [
+                  TImages.userProfileImage4,
+                  TImages.userProfileImage4,
+                  TImages.userProfileImage4,
+                ],
+              ),
+              const PartnertShowcase(
+                images: [
+                  TImages.userProfileImage4,
+                  TImages.userProfileImage4,
+                  TImages.userProfileImage4,
+                ],
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
 
-                                // Text
-                                Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const DoctorTitleWithVerified(
-                                        title: 'POLIJE',
-                                        doctorTextSize: TextSizes.medium,
-                                      ),
-                                      Text(
-                                        '200+ koas available',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .labelMedium!,
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    )
-                  ],
+              // Posts
+              SectionHeading(title: 'You might like', onPressed: () {}),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              DGridLayout(
+                itemCount: 4,
+                crossAxisCount: 1,
+                itemBuilder: (_, index) => const DoctorCard(
+                  doctorName: 'Dr. John Doe',
+                  specialty: 'Dentist',
+                  distance: '2 km',
+                  rating: 4.5,
+                  reviewsCount: 120,
+                  openTime: '9:00 AM - 5:00 PM',
+                  doctorImageUrl: TImages.userProfileImage4,
                 ),
               ),
-            ),
-          ];
-        },
-        body: Container(),
-      ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
