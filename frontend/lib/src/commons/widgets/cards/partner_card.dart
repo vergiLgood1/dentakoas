8e7eab2a8104979c1a5851ef5283477661a7283c
vergiLgood1 <xdamazon17@gmail.com>
2024-12-24 14:00:47 +0700

    Update partner_card.dart

--- frontend/lib/src/commons/widgets/cards/partner_card.dart
+++ frontend/lib/src/commons/widgets/cards/partner_card.dart
@@ -1,35 +1,41 @@
 import 'package:denta_koas/src/commons/widgets/containers/rounded_container.dart';
-import 'package:denta_koas/src/commons/widgets/text/doctor_title_with_verified.dart';
+import 'package:denta_koas/src/commons/widgets/text/title_with_verified.dart';
 import 'package:denta_koas/src/utils/constants/colors.dart';
 import 'package:denta_koas/src/utils/constants/enums.dart';
 import 'package:denta_koas/src/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
-class PartnertCard extends StatelessWidget {
-  const PartnertCard({
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
     super.key,
     this.showBorder = true,
     this.onTap,
     required this.title,
-    required this.subtitle,
-    required this.image,
+    this.subtitle,
+    this.image,
     this.isNetworkImage = false,
     this.doctorTextSize = TextSizes.medium,
     this.backgroundColor = TColors.transparent,
     this.maxLines = 1,
     this.textAlign = TextAlign.center,
+    this.showVerifiyIcon = true,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.bordeColor = TColors.borderPrimary,
   });
 
   final bool showBorder;
+  final bool showVerifiyIcon;
   final void Function()? onTap;
   final String title;
-  final String subtitle;
-  final String image;
+  final String? subtitle;
+  final String? image;
   final bool isNetworkImage;
   final TextSizes doctorTextSize;
   final Color backgroundColor;
   final int maxLines;
   final TextAlign textAlign;
+  final CrossAxisAlignment crossAxisAlignment;
+  final Color bordeColor;
 
   @override
   @override
@@ -39,32 +45,46 @@ class PartnertCard extends StatelessWidget {
       child: RoundedContainer(
         padding: const EdgeInsets.all(TSizes.sm),
         showBorder: showBorder,
+        borderColor: bordeColor,
         backgroundColor: TColors.transparent,
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CircleAvatar(
+            if (image != null) 
+              CircleAvatar(
               radius: 24,
               backgroundColor: backgroundColor,
-              backgroundImage:
-                  isNetworkImage ? NetworkImage(image) : AssetImage(image),
-            ),
+                backgroundImage: isNetworkImage
+                    ? NetworkImage(image!)
+                    : AssetImage(image!) as ImageProvider,
+              ),
             const SizedBox(width: TSizes.spaceBtwItems / 2),
 
             // Expanded for flexible text
             Expanded(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: crossAxisAlignment,
                 children: [
-                  DoctorTitleWithVerified(
+                  if (showVerifiyIcon)
+                    TitleWithVerified(
                     title: title,
                     doctorTextSize: doctorTextSize,
                     maxLines: maxLines,
                     textAlign: textAlign,
+                    )
+                  else
+                    TitleWithVerified(
+                      title: title,
+                      doctorTextSize: doctorTextSize,
+                      maxLines: maxLines,
+                      textAlign: textAlign,
+                      showIcon: false,
                   ),
+                  
+                  if (subtitle != null)
                   Text(
-                    subtitle,
+                      subtitle!,
                     overflow: TextOverflow.ellipsis,
                     style: Theme.of(context).textTheme.labelMedium!,
                   ),
