3e20274e8f9ed58bf18348ef210f8aace6625551
vergiLgood1 <xdamazon17@gmail.com>
2024-12-25 23:41:01 +0700

    Update time_controller.dart

--- frontend/lib/src/features/appointment/controller/time_controller.dart
+++ frontend/lib/src/features/appointment/controller/time_controller.dart
@@ -1,11 +1,11 @@
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
-class TimeController extends GetxController {
-  static TimeController get instance => Get.find();
+// class TimeController extends GetxController {
+//   static TimeController get instance => Get.find();
 
-  final selectedTimeStamp = (-1).obs;
+//   final selectedTimeStamp = (-1).obs;
 
-  void updateSelectedTimeStamp(int index) {
-    selectedTimeStamp.value = index;
-  }
-}
+//   void updateSelectedTimeStamp(int index) {
+//     selectedTimeStamp.value = index;
+//   }
+// }
