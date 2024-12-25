a8baaf24b6e8d937d5c729ba5e11153f3d6dcf70
vergiLgood1 <xdamazon17@gmail.com>
2024-12-25 23:41:13 +0700

    Create overal_koas_rating.dart

---
+++ frontend/lib/src/features/appointment/screen/koas_reviews/widgets/overal_koas_rating.dart
@@ -0,0 +1,50 @@
+import 'package:denta_koas/src/features/appointment/screen/koas_reviews/widgets/rating_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class OverallKoasRating extends StatelessWidget {
+  const OverallKoasRating({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Text(
+            '4.8',
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+        ),
+        const Expanded(
+          flex: 7,
+          child: Column(
+            children: [
+              RatingProgressIndicator(
+                text: '5',
+                value: 1.0,
+              ),
+              RatingProgressIndicator(
+                text: '4',
+                value: 0.8,
+              ),
+              RatingProgressIndicator(
+                text: '3',
+                value: 0.6,
+              ),
+              RatingProgressIndicator(
+                text: '2',
+                value: 0.4,
+              ),
+              RatingProgressIndicator(
+                text: '1',
+                value: 0.2,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
