10fa8de037ecdc4c558cb1e52e0ef8ff4a5b948d
vergiLgood1 <xdamazon17@gmail.com>
2024-12-25 23:41:15 +0700

    Create rating_progress_indicator.dart

---
+++ frontend/lib/src/features/appointment/screen/koas_reviews/widgets/rating_progress_indicator.dart
@@ -0,0 +1,42 @@
+import 'package:denta_koas/src/utils/constants/colors.dart';
+import 'package:denta_koas/src/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+
+class RatingProgressIndicator extends StatelessWidget {
+  const RatingProgressIndicator({
+    super.key,
+    required this.text,
+    required this.value,
+  });
+
+  final String text;
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        Expanded(
+          flex: 11,
+          child: SizedBox(
+            width: TDeviceUtils.getScreenWidth(context) * 0.8,
+            child: LinearProgressIndicator(
+              value: value,
+              minHeight: 11,
+              backgroundColor: TColors.grey,
+              borderRadius: BorderRadius.circular(7),
+              valueColor: const AlwaysStoppedAnimation(TColors.primary),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
