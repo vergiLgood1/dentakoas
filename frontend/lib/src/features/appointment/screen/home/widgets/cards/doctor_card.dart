83fc6149504d4179fe9986d6866a980401521840
vergiLgood1 <xdamazon17@gmail.com>
2024-12-26 23:56:15 +0700

    Update doctor_card.dart

--- frontend/lib/src/features/appointment/screen/home/widgets/cards/doctor_card.dart
+++ frontend/lib/src/features/appointment/screen/home/widgets/cards/doctor_card.dart
@@ -10,6 +10,7 @@ class DoctorCard extends StatelessWidget {
   final int reviewsCount;
   final String openTime;
   final String doctorImageUrl;
+  final void Function()? onTap;
 
   const DoctorCard({
     super.key,
@@ -20,161 +21,166 @@ class DoctorCard extends StatelessWidget {
     required this.reviewsCount,
     required this.openTime,
     required this.doctorImageUrl,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(
-          bottom: TSizes.spaceBtwItems), // Adjust spacing between cards
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: TColors.textWhite,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 8,
-            offset: Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Image Profile
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color:
-                      Colors.grey[200], // Placeholder color for missing image
-                  child: Image.asset(
-                    doctorImageUrl,
-                    fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+            bottom: TSizes.spaceBtwItems), // Adjust spacing between cards
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: TColors.textWhite,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Image Profile
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color:
+                        Colors.grey[200], // Placeholder color for missing image
+                    child: Image.asset(
+                      doctorImageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: TSizes.spaceBtwItems),
+                const SizedBox(width: TSizes.spaceBtwItems),
 
-              // Doctor Details
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Doctor Name
-                    Text(
-                      doctorName,
-                      style: Theme.of(context).textTheme.titleMedium!.apply(
-                            color: TColors.textPrimary,
+                // Doctor Details
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Doctor Name
+                      Text(
+                        doctorName,
+                        style: Theme.of(context).textTheme.titleMedium!.apply(
+                              color: TColors.textPrimary,
+                            ),
+                      ),
+                      const SizedBox(height: 4),
+
+                      // Specialty and Distance
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              specialty,
+                              style:
+                                  Theme.of(context).textTheme.bodyMedium!.apply(
+                                        color: TColors.textSecondary,
+                                      ),
+                            ),
                           ),
-                    ),
-                    const SizedBox(height: 4),
-
-                    // Specialty and Distance
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            specialty,
+                          const Icon(Icons.location_on,
+                              size: TSizes.iconSm, color: TColors.primary),
+                          Text(
+                            distance,
                             style:
-                                Theme.of(context).textTheme.bodyMedium!.apply(
+                                Theme.of(context).textTheme.labelMedium!.apply(
                                       color: TColors.textSecondary,
                                     ),
                           ),
-                        ),
-                        const Icon(Icons.location_on,
-                            size: TSizes.iconSm, color: TColors.primary),
-                        Text(
-                          distance,
-                          style: Theme.of(context).textTheme.labelMedium!.apply(
-                                color: TColors.textSecondary,
+                        ],
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwItems),
+
+                      // Rating and Open Time
+                      Row(
+                        children: [
+                          // Rating Section
+                          Row(
+                            children: [
+                              const Icon(Icons.star,
+                                  size: TSizes.iconBase, color: Colors.orange),
+                              const Icon(Icons.star,
+                                  size: TSizes.iconBase, color: Colors.orange),
+                              const Icon(Icons.star,
+                                  size: TSizes.iconBase, color: Colors.orange),
+                              const Icon(Icons.star,
+                                  size: TSizes.iconBase, color: Colors.orange),
+                              const Icon(Icons.star,
+                                  size: TSizes.iconBase, color: Colors.orange),
+                              const SizedBox(width: 4),
+                              Text(
+                                '$rating',
+                                style: const TextStyle(
+                                  fontSize: TSizes.fontSizeSm,
+                                  color: TColors.textPrimary,
+                                ),
                               ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwItems),
-
-                    // Rating and Open Time
-                    Row(
-                      children: [
-                        // Rating Section
-                        Row(
-                          children: [
-                            const Icon(Icons.star,
-                                size: TSizes.iconBase, color: Colors.orange),
-                            const Icon(Icons.star,
-                                size: TSizes.iconBase, color: Colors.orange),
-                            const Icon(Icons.star,
-                                size: TSizes.iconBase, color: Colors.orange),
-                            const Icon(Icons.star,
-                                size: TSizes.iconBase, color: Colors.orange),
-                            const Icon(Icons.star,
-                                size: TSizes.iconBase, color: Colors.orange),
-                            const SizedBox(width: 4),
-                            Text(
-                              '$rating',
-                              style: const TextStyle(
-                                fontSize: TSizes.fontSizeSm,
-                                color: TColors.textPrimary,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Spacer(),
-
-                        // Divider
-                        Container(
-                          width: 2,
-                          height: TSizes.iconBase,
-                          color: TColors.grey,
-                        ),
-                        const SizedBox(width: 6),
-
-                        // Open Time
-                        Text(
-                          '$reviewsCount Reviews',
-                          style: const TextStyle(
-                            fontSize: TSizes.fontSizeSm,
-                            color: TColors.textSecondary,
+                            ],
                           ),
-                        ),
-                      ],
+                          const Spacer(),
+
+                          // Divider
+                          Container(
+                            width: 2,
+                            height: TSizes.iconBase,
+                            color: TColors.grey,
+                          ),
+                          const SizedBox(width: 6),
+
+                          // Open Time
+                          Text(
+                            '$reviewsCount Reviews',
+                            style: const TextStyle(
+                              fontSize: TSizes.fontSizeSm,
+                              color: TColors.textSecondary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+
+            // Full-width Button
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                  ],
+                    side: const BorderSide(
+                        color: TColors.transparent, width: 1.5),
+                    backgroundColor:
+                        TColors.primary.withAlpha((0.2 * 255).toInt())),
+                child: const Text(
+                  'Make Appointment',
+                  style: TextStyle(
+                      fontSize: TSizes.fontSizeSm, color: TColors.primary),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 12),
-
-          // Full-width Button
-          SizedBox(
-            width: double.infinity,
-            child: OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  side:
-                      const BorderSide(color: TColors.transparent, width: 1.5),
-                  backgroundColor:
-                      TColors.primary.withAlpha((0.2 * 255).toInt())),
-              child: const Text(
-                'Make Appointment',
-                style: TextStyle(
-                    fontSize: TSizes.fontSizeSm, color: TColors.primary),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
