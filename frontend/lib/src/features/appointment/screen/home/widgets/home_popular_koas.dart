21d06d26d61e27c5a4aee43040f78fcd6924766b
vergiLgood1 <xdamazon17@gmail.com>
2024-12-26 23:56:23 +0700

    Update home_popular_koas.dart

--- frontend/lib/src/features/appointment/screen/home/widgets/home_popular_koas.dart
+++ frontend/lib/src/features/appointment/screen/home/widgets/home_popular_koas.dart
@@ -2,6 +2,7 @@ import 'package:denta_koas/src/commons/widgets/layouts/grid_layout.dart';
 import 'package:denta_koas/src/commons/widgets/text/section_heading.dart';
 import 'package:denta_koas/src/features/appointment/screen/dentist/all_koas.dart';
 import 'package:denta_koas/src/features/appointment/screen/home/widgets/cards/doctor_card.dart';
+import 'package:denta_koas/src/features/appointment/screen/koas/koas_details/koas_detail.dart';
 import 'package:denta_koas/src/utils/constants/image_strings.dart';
 import 'package:denta_koas/src/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -29,7 +30,9 @@ class HomePopularKoasSection extends StatelessWidget {
           // Popular Koas
           DGridLayout(
             itemCount: 2,
-            itemBuilder: (_, index) => const DoctorCard(
+            crossAxisCount: 1,
+            itemBuilder: (_, index) => DoctorCard(
+                onTap: () => Get.to(() => const KoasDetailScreen()),
                 doctorName: 'Dr. Joseph Brostito',
                 specialty: 'Dental Specialist',
                 distance: '1 KM',
@@ -37,7 +40,6 @@ class HomePopularKoasSection extends StatelessWidget {
                 reviewsCount: 120,
                 openTime: '17.00',
                 doctorImageUrl: TImages.userProfileImage3),
-            crossAxisCount: 1,
           ),
         ],
       ),
