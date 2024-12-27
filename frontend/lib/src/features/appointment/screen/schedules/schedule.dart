863eae5062cf812d5391911c6c208c5614c3f4f3
vergiLgood1 <xdamazon17@gmail.com>
2024-12-27 15:08:09 +0700

    Update schedule.dart
    Add custom tab bar

--- frontend/lib/src/features/appointment/screen/schedules/schedule.dart
+++ frontend/lib/src/features/appointment/screen/schedules/schedule.dart
@@ -1,3 +1,6 @@
+import 'package:denta_koas/src/commons/widgets/appbar/tabbar.dart';
+import 'package:denta_koas/src/utils/constants/sizes.dart';
+import 'package:denta_koas/src/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleScreen extends StatelessWidget {
@@ -5,6 +8,69 @@ class ScheduleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final appBarHeight = TDeviceUtils.getAppBarHeight();
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 1,
+      child: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(height: appBarHeight), // Menambahkan jarak ke bawah
+            TabBarApp(
+              splashFactory: NoSplash.splashFactory,
+              splashBorderRadius: BorderRadius.circular(30.0),
+              dividerColor: Colors.transparent,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: BoxDecoration(
+                color: Colors.blue.shade50,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              isScrollable: true,
+              tabAlignment: TabAlignment.center,
+              tabs: [
+                Tab(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: TSizes.spaceBtwItems), // Margin antar tab
+                    child: const Text('Canceled Schedule'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: TSizes.spaceBtwItems),
+                    child: const Text('Upcoming Schedule'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: TSizes.spaceBtwItems),
+                    child: const Text('Ongoing Schedule'),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: TSizes.spaceBtwItems),
+                    child: const Text('Completed Schedule'),
+                  ),
+                ),
+              ],
+            ),
+            const Expanded(
+              child: TabBarView(
+                children: [
+                  Center(child: Text('Canceled')),
+                  Center(child: Text('Upcoming')),
+                  Center(child: Text('Ongoing')),
+                  Center(child: Text('Completed')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
