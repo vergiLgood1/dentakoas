302f22dedcaf1a061db55f6e4d73e926c622bb1d
vergiLgood1 <xdamazon17@gmail.com>
2024-12-28 23:03:41 +0700

    add locale

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:denta_koas/src/features/onboarding/screen/onboarding/onboarding.
 import 'package:denta_koas/src/utils/constants/text_strings.dart';
 import 'package:denta_koas/src/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
@@ -15,6 +16,10 @@ class App extends StatelessWidget {
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      supportedLocales: const [
+        Locale('id', ''),
+      ],
       // initialBinding: GeneralBindings(),
       home: const OnBoardingScreen(),
     );
