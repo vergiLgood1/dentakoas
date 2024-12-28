be435ec851692cef01403e88572cbb65b200a707
vergiLgood1 <xdamazon17@gmail.com>
2024-12-28 23:03:45 +0700

    Update navigation_menu.dart

--- frontend/lib/navigation_menu.dart
+++ frontend/lib/navigation_menu.dart
@@ -1,5 +1,6 @@
 import 'package:denta_koas/src/features/appointment/screen/explore/explore.dart';
 import 'package:denta_koas/src/features/appointment/screen/home/home.dart';
+import 'package:denta_koas/src/features/appointment/screen/posts/create_post/create_post.dart';
 import 'package:denta_koas/src/features/appointment/screen/schedules/schedule.dart';
 import 'package:denta_koas/src/features/personalization/screen/setting/settings.dart';
 import 'package:flutter/material.dart';
@@ -14,67 +15,76 @@ class NavigationMenu extends StatelessWidget {
     final controller = Get.put(NavigationController());
     return Scaffold(
       bottomNavigationBar: Obx(
-        () => Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24,
-            vertical: 16,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withAlpha(0.3 * 255 ~/ 100),
-                offset: const Offset(0, -2),
-                blurRadius: 4,
-                spreadRadius: 0,
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: controller.displayedScreens.asMap().entries.map((entry) {
-              final index = entry.key;
-              final isActive = controller.selectedIndex.value == index;
+        () {
+          // final index = controller.selectedIndex.value;
+          // // Sembunyikan bottom navigation bar pada CreatePostScreen
+          // if (controller.displayedScreens[index] is CreatePostScreen) {
+          //   return const SizedBox
+          //       .shrink(); // Tidak menampilkan bottomNavigationBar
+          // }
+          return Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 16,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withAlpha(0.3 * 255 ~/ 100),
+                  offset: const Offset(0, -2),
+                  blurRadius: 4,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:
+                  controller.displayedScreens.asMap().entries.map((entry) {
+                final index = entry.key;
+                final isActive = controller.selectedIndex.value == index;
 
-              return GestureDetector(
-                onTap: () {
-                  controller.selectedIndex.value = index;
-                },
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: isActive ? Colors.blue.shade50 : null,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        controller.displayedIcons[index],
-                        color: isActive ? Colors.blue : Colors.grey,
-                        size: 24,
-                      ),
-                      if (isActive)
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: Text(
-                            controller.displayedLabels[index],
-                            style: const TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.w500,
+                return GestureDetector(
+                  onTap: () {
+                    controller.selectedIndex.value = index;
+                  },
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isActive ? Colors.blue.shade50 : null,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          controller.displayedIcons[index],
+                          color: isActive ? Colors.blue : Colors.grey,
+                          size: 24,
+                        ),
+                        if (isActive)
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              controller.displayedLabels[index],
+                              style: const TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
-                        ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+                );
+              }).toList(),
+            ),
+          );
+        },
       ),
       body: Obx(() {
         final index = controller.selectedIndex.value;
@@ -85,6 +95,7 @@ class NavigationMenu extends StatelessWidget {
 }
 
 
+
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
@@ -93,9 +104,7 @@ class NavigationController extends GetxController {
     const ExploreScreen(),
     const ScheduleScreen(),
     const SettingsScreen(),
-    Container(
-      color: Colors.red,
-    ),
+    const CreatePostScreen()
   ];
 
   final List<IconData> icons = [
