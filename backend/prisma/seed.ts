55546b9a78b3a6f360a3fc7c9bcb3675b269117c
vergiLgood1 <xdamazon17@gmail.com>
2025-01-03 22:58:29 +0700

    Update seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -41,16 +41,84 @@ async function main() {
   //   }
   // }
 
-  for (const treatmentType of TreatmentTypes) {
-    const createdTreatmentType = await prisma.treatmentType.create({
-      data: {
-        ...treatmentType,
-        name: treatmentType.name,
-      },
-    });
-  }
+  // for (const treatmentType of TreatmentTypes) {
+  //   const createdTreatmentType = await prisma.treatmentType.create({
+  //     data: {
+  //       ...treatmentType,
+  //       name: treatmentType.name,
+  //     },
+  //   });
+  // }
 
-  console.log("Data seed added:", TreatmentTypes);
+  const universities = [
+    {
+      name: "Universitas Indonesia",
+      alias: "UI",
+      location: "Depok, Jawa Barat",
+    },
+    {
+      name: "Institut Teknologi Bandung",
+      alias: "ITB",
+      location: "Bandung, Jawa Barat",
+    },
+    { name: "Universitas Gadjah Mada", alias: "UGM", location: "Yogyakarta" },
+    {
+      name: "Institut Pertanian Bogor",
+      alias: "IPB",
+      location: "Bogor, Jawa Barat",
+    },
+    {
+      name: "Universitas Airlangga",
+      alias: "UNAIR",
+      location: "Surabaya, Jawa Timur",
+    },
+    {
+      name: "Universitas Brawijaya",
+      alias: "UB",
+      location: "Malang, Jawa Timur",
+    },
+    {
+      name: "Universitas Diponegoro",
+      alias: "UNDIP",
+      location: "Semarang, Jawa Tengah",
+    },
+    {
+      name: "Universitas Padjadjaran",
+      alias: "UNPAD",
+      location: "Bandung, Jawa Barat",
+    },
+    {
+      name: "Universitas Sebelas Maret",
+      alias: "UNS",
+      location: "Surakarta, Jawa Tengah",
+    },
+    {
+      name: "Universitas Sumatera Utara",
+      alias: "USU",
+      location: "Medan, Sumatera Utara",
+    },
+    {
+      name: "Politeknik Negeri Jember",
+      alias: "POLIJE",
+      location: "Jember, Jawa Timur",
+    },
+    {
+      name: "Universitas Negeri Jember",
+      alias: "UNEJ",
+      location: "Jember, Jawa Timur",
+    },
+    // Tambahkan lebih banyak universitas sesuai kebutuhan
+  ];
+
+  // Menggunakan createMany untuk menambahkan banyak entri sekaligus
+  await prisma.university.createMany({
+    data: universities,
+    skipDuplicates: true, // Menghindari error jika data sudah ada
+  });
+
+  console.log("Seeded universities successfully." + universities);
+
+  // console.log("Data seed added:", TreatmentTypes);
 }
 
 main()
