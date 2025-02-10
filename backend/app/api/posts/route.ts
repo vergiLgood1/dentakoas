fcab6b3c1cae70f9f8487b185a230a16621e0bcc
vergiLgood1 <xdamazon17@gmail.com>
2025-02-11 01:00:12 +0700

    Update route.ts

--- backend/app/api/posts/route.ts
+++ backend/app/api/posts/route.ts
@@ -87,10 +87,7 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function POST(req: Request) {
   const body = await req.json();
   const {
     userId,
